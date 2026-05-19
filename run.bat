ceb25e6f974e686c3af56ee71fccbb99145e7502
FATIHI-root <yasserfatihi03@gmail.com>
2026-05-19 21:45:31 +0100

    Delete run.bat

--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java --module-path "target\lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.base -cp "target\GestionHandicap-1.0-SNAPSHOT.jar;target\lib\*" ma.ac.uir.gestionhandicap.MainApp
-pause
